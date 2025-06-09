71b497d76d846f98699d7287e7aa2d1b8598a9ca
Zeuxsf <alexandresf8105@gmail.com>
2025-06-09 10:22:47 -0300

    infos

--- InnovaPure.py
+++ InnovaPure.py
@@ -1,14 +1,21 @@
 import re
 from fuzzywuzzy import process
+import json
 
-banco_dados = {
-    "Bom dia, Boa Tarde, Boa Noite": "Olá! Como posso ajudar você hoje??",
-    "oi, olá": "Olá, tudo bem?",
-    "Qual o seu nome? como você se chama? é você? você": "InnovaPure! O chatbot de atendimento da Innova-T.O.",
-    "Quem é O CEO da Innova?, fundador da innova, fundou a innova": "O Fundador e CEO da Innova é Lucas Jaeger!",
-    "seu Criador? quem é seu criador? criador, criado, Quem te criou?": "Meu criador se chama Alexandre França!",
-    "atendido, atendimento, atender, Quero marcar um horário": "Para marcar um horário, basta digitar seu número!"
-}
+dados =  {
+  "Bom dia, Boa Tarde, Boa Noite": "Olá! Como posso ajudar você hoje??",
+  "oi, olá": "Olá, tudo bem?",
+  "Qual o seu nome? como você se chama? é você? você, quem é vc? quem é você?": "InnovaPure! O chatbot de atendimento da Innova-T.O.",
+  "é você? você, quem é vc? quem é você?": "InnovaPure! O chatbot de atendimento da Innova-T.O.",
+  "Quem é O CEO da Innova?, fundador da innova, fundou a innova": "O Fundador e CEO da Innova é Lucas Jaeger!",
+  "seu Criador? quem é seu criador? criador, criado, Quem te criou?": "Meu criador se chama Alexandre França!",
+  "atendido, atendimento, atender, Quero marcar um horário": "Para marcar um horário, basta digitar seu número!",
+  "Quantas pessoas trabalham na innova? pessoas, funcionários, quantos funcionários a innova tem?": "A Innova contém uma equipe de mais de 20 profissionais Altamente Qualificados!",
+  "Oque é a Innova? com oque a innova trabalha? oque a empresa faz?, oque a innova faz?": "Transformamos ideias em Realidade Digital! Temos Soluções em software que vão Potencializar o seu negócio!",
+  "oque a innova faz? oque é a innova T.O.? Quem é a innova? innova tech one": "Transformamos ideias em Realidade Digital! Temos Soluções em software que vão Potencializar o seu negócio!",
+  "Qual é o instagram da innova?insta, Qual é o insta de vcs? ,Vocês possuem alguma rede social? meio de contato, perfil, instagram": "Acesse nosso perfil no Instagram: @innovatech.one e entre em contato Conosco!",
+  "Obrigado, agradeço, valeu, tmj": "Que bom que eu pude te ajudar :D"
+  }
 
 def marcar_horario(input_do_usuario):
     padrao_de_telefone = r'\d{2}\d{5}\d{4}'
@@ -21,9 +28,9 @@ def responder(input_do_usuario):
     if marcar:
         return marcar
     
-    melhor_resposta = process.extractOne(input_do_usuario,banco_dados.keys())
-    if melhor_resposta[1] > 50:
-        return banco_dados[melhor_resposta[0]]
+    melhor_resposta = process.extractOne(input_do_usuario,dados.keys())
+    if melhor_resposta[-1] > 80:
+        return dados[melhor_resposta[0]]
     else:
         return "Desculpe, não entendi sua pergunta. Poderia repetir?"
 
